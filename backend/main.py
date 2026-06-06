74b5363f66df3654e61fb6dda1191240f5b16870
malav <makwanamalav890@gmail.com>
2026-06-06 21:18:01 +0530

    feat: Implement JWT auth, User models, and bcrypt hashing

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from database import engine, Base
+from models import user
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Document Signature API")
 
 @app.get("/")
 def read_root():
-    return {"message": "Backend is running securely."}
+    return {"message": "Backend is running securely."} 
